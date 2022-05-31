ad3e45c129aaeb1ec484735c3f2c0544752370aa
tanvirss13 <79608956+tanvirss13@users.noreply.github.com>
2022-05-31 15:44:20 -0700

    Add files via upload

---
+++ ufo_functions.cpp
@@ -0,0 +1,16 @@
+//
+//  ufo_functions.cpp
+//  UFO_Game
+//
+//  Created by Tanvir Sethi on 5/31/22.
+//
+
+
+// ufo_functions.hpp
+#include <vector>
+#include <stdio.h>
+void display_misses(int misses);
+// Declare functions
+void greet();
+void end_game(std::string answer, std::string codeword);
+void display_status(std::vector<char> incorrect, std::string answer);
